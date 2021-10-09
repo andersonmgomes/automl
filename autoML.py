1972c296b7813f4fb61abeaaa3363e66c1f696c1
Anderson Martins <andersonmg@gmail.com>
2021-10-09 07:44:25 -0300

    new metrics and algorithms

--- autoML.py
+++ autoML.py
@@ -1,54 +1,67 @@
+import sklearn
 from sklearn.base import ClassifierMixin, RegressorMixin
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import r2_score, mean_squared_error, f1_score
+from sklearn.model_selection import cross_val_score
 from sklearn import linear_model
 import numpy as np
 from itertools import chain, combinations
 from sklearn import preprocessing
 from sklearn import svm
 from sklearn import tree
-import pandas as pd
-
-METRICS_R2 = 'R2'
-METRICS_MAE = 'MAE'
-METRICS_MSE = 'MSE'
-METRICS_F1 = 'F1'
+from sklearn import neighbors
+import pandas as pd 
 
 class AutoML:
     def __init__(self, ds, y_colname 
-                 , algorithms = [linear_model.LinearRegression(), svm.SVR(), tree.DecisionTreeRegressor()]
+                 , algorithms = [linear_model.LinearRegression(), svm.SVR(), tree.DecisionTreeRegressor()
+                                 , neighbors.KNeighborsRegressor(), linear_model.LogisticRegression()
+                                 , svm.SVC(), neighbors.KNeighborsClassifier(), tree.DecisionTreeClassifier()]
                  , unique_categoric_limit = 15) -> None:
-        self.__unique_categoric_limit = unique_categoric_limit
         self.__ds_full = ds
         self.__ds_onlynums = self.__ds_full.select_dtypes(exclude=['object'])
         self.__X_full = self.__ds_onlynums.drop(columns=[y_colname])
         self.__Y_full = self.__ds_onlynums[[y_colname]]
         self.__results = None
         self.algorithms = algorithms
+        self.__unique_categoric_limit = unique_categoric_limit
+        self.METRICS_REGRESSION = ['r2', 'neg_mean_absolute_error', 'neg_mean_squared_error']
+        self.METRICS_CLASSIFICATION = ['f1', 'accuracy', 'roc_auc']
+        # metrics reference:
+        # https://scikit-learn.org/stable/modules/model_evaluation.html
         
     def addAlgorithm(self, algo):
         self.algorithms.append(algo)
         self.__results = None #cleaning the previous results
     
     def getBestModel(self):
-        if self.getBestResult() is None:
+        if self.getBestResult(True) is None:
             return None
         #else
-        return self.getBestResult().model_instance
+        return self.getBestResult(True).model_instance
 
-    def getBestResult(self):
-        if len(self.getResults()) == 0:
+    def getBestResult(self, resultWithModel=False):
+        if len(self.getResults(resultWithModel)) == 0:
             return None
         #else
-        return self.getResults().iloc[0]
+        return self.getResults(resultWithModel).iloc[0]
     
-    def getResults(self, buffer=True):
+    def getResults(self, resultWithModel=False, buffer=True):
         if buffer and self.__results is not None:
-            return self.__results
+            if resultWithModel:                   
+                return self.__results
+            #else
+            return self.__results.drop('model_instance', axis=1)
+                   
         #else to get results
-        #[algorithm, x_cols, mae, r2, mse, model]
-        self.__results = pd.DataFrame(columns=['algorithm', 'features', METRICS_F1, METRICS_MAE, METRICS_R2, METRICS_MSE, 'model_instance'])
+        #dataframe format: [algorithm, x_cols,[metrics], model_instance]
+        columns_list = ['algorithm', 'features']
+        if self.YisCategorical():
+            columns_list.extend(self.METRICS_CLASSIFICATION)
+        else:
+            columns_list.extend(self.METRICS_REGRESSION)
+        columns_list.append('model_instance')
+        
+        self.__results = pd.DataFrame(columns=columns_list)
 
         y_is_cat = self.YisCategorical()
         y_is_num = not y_is_cat
@@ -63,14 +76,17 @@ class AutoML:
                 self.__results.loc[len(self.__results)] = self.__score_dataset(algo, col_tuple)
         
         self.__results.set_index(['algorithm', 'features'])
-        sortby = METRICS_R2
-        
+
+        sortby = self.METRICS_REGRESSION[0] #considering the first element the most important
         if y_is_cat:
-            sortby = METRICS_F1
+            sortby = self.METRICS_CLASSIFICATION[0] #considering the first element the most important
             
         self.__results.sort_values(by=sortby, ascending=False, inplace=True)
-                            
-        return self.__results           
+        
+        if resultWithModel:                   
+            return self.__results
+        #else
+        return self.__results.drop('model_instance', axis=1)           
     
     def YisCategorical(self) -> bool:
         y_type = type(self.__Y_full.iloc[0,0])
@@ -97,7 +113,7 @@ class AutoML:
         X_normal = min_max_scaler.fit_transform(X)
         y_normal = min_max_scaler.fit_transform(y)
         
-        X_train, X_valid, y_train, y_valid = train_test_split(X_normal, y_normal, train_size=0.8, test_size=0.2, random_state=0)
+        X_train, X_valid, y_train, y_valid = train_test_split(X_normal, y_normal, train_size=0.8, test_size=0.2, random_state=1102)
         
         model = algorithm
 
@@ -115,25 +131,34 @@ class AutoML:
         model.fit(X_train2, y_train2.ravel())
         preds = model.predict(X_valid2)
         
-        mae = mean_absolute_error(y_valid2, preds)
-        r2 = r2_score(y_valid2, preds)
-        mse = mean_squared_error(y_valid2, preds)
-        f1 = None
+        scoring_list = self.METRICS_REGRESSION
         if self.YisCategorical():
-            f1 = f1_score(y_valid2, preds)
-            
-        return np.array([str(algorithm).replace('()',''), x_cols, f1, mae, r2, mse, model], dtype=object)
+            scoring_list = self.METRICS_CLASSIFICATION
+        
+        metrics_value_list = []
+        
+        for scor in scoring_list:
+            metrics_value_list.append(np.mean(cross_val_score(model, X_valid2, y_valid2.ravel(), cv=5, scoring=scor)))
+        
+        result_list =  [str(algorithm).replace('()',''), x_cols]
+        result_list.extend(metrics_value_list)
+        result_list.append(model)       
+        return np.array(result_list, dtype=object)
 
 #util methods
 def all_subsets(ss):
     return chain(*map(lambda x: combinations(ss, x), range(0, len(ss)+1)))
 
-'''4 Tests
+#4 Tests
+#print(sorted(sklearn.metrics.SCORERS.keys()))
+'''
 import ds_utils as ut
-ds_hhprice = ut.getDSPriceHousing_ClassProb()
-from sklearn import svm
-algorithms = [svm.SVC()]
-automl_house = AutoML(ds_hhprice, 'high_price', algorithms=algorithms)
-print(automl_house.getResults())
+ds_house_class = ut.getDSPriceHousing_ClassProb()
+automl_house_class = AutoML(ds_house_class, 'high_price')
+print(automl_house_class.getResults().head(10))
+
+ds_house_regr = ut.getDSPriceHousing()
+automl_house_regr = AutoML(ds_house_regr, 'Price')
+print(automl_house_regr.getResults().head(10))
 '''
 
